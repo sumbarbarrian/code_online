bc930a0a52d2b7edf28619a1112f9cfbfee186d6
sumbarbarrian <Titanium1>
2021-07-09 19:19:35 +0300

    added comments

--- server/server.js
+++ server/server.js
@@ -2,11 +2,19 @@ const express = require('express')
 const ws = require('ws')
 const server = express()
 
+
+
+/**
+ *  this endpoint opens session
+ */
 server.get('/create', (req, res) => {
     console.log('create session')
     res.send()
 })
 
+/**
+ *  this endpoint closes session
+ */
 server.get('/close', (req, res) => {
     console.log('close session')
     res.send()
