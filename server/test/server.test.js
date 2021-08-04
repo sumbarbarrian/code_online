9674938b7e3509529d58f602f91d8d25afa837ed
Elizaveta Antonova <lizaantonova1@yandex.ru>
2021-08-04 21:06:15 +0300

    new tests

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,6 +1,7 @@
 const expect = require("chai").expect
 const request = require('supertest')
 const server = require('../server')
+const assert = require("assert");
 
 let app
 
@@ -23,4 +24,27 @@ describe('server.js', () => {
                 expect(body).to.have.property('closeToken')
             }).end(done)
     })
+
+    it  ('/close/:closeid returns 200', async () => {
+        const response = await request(app)
+            .get('/create')
+        const json = JSON.parse(response.text)
+        const responseCloseId = await request(app)
+            .get('/close/' + json.closeToken)
+        expect(responseCloseId.statusCode).equals(201)
+    })
+
+    it  ('/close/:closeid returns 404', async () => {
+        const responseCloseId = await request(app)
+            .get('/close/l')
+        expect(responseCloseId.statusCode).equals(404)
+        expect(responseCloseId.text).to.not.equals("")
+        expect(JSON.parse(responseCloseId.text)).to.have.property('errorMessage', 'Session does not exist')
+    })
+
+
+
+
+
+
 })
